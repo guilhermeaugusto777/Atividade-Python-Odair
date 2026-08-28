7f4c84af63d2081521c23cccbf804d3f44d10324
Guilherme Augusto <guilhermeafernando@gmail.com>
2026-08-27 21:22:47 -0300

    Delete Atividade 6 - Área e perímetro do retangulo.py

--- Atividade 6 - Área e perímetro do retangulo.py
+++
@@ -1,8 +0,0 @@
-largura = float(input("Largura: ").replace(",", "."))
-altura = float(input("Altura: ").replace(",", "."))
-
-area = largura * altura
-perimetro = 2 * (largura + altura)
-
-print(f"Área: {area:g}")
-print(f"Perímetro: {perimetro:g}")
